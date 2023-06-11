ef1c88826cbeea2fc0e305acdb5ca1a911ddfbf6
Auporna-Barua <aupornabarua@gmail.com>
2023-06-11 22:22:50 +0600

    route add in our project with help of react router hook

---
+++ src/Routers/PrivateRoute.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../Provider/AuthProvider';
+
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+    const location = useLocation()
+
+    if (user) {
+        return children
+    }
+    if (loading) {
+        return <div className='flex justify-center items-center h-screen'>
+            <span className="loading loading-bars loading-lg"></span>
+        </div>
+    }
+
+    return <Navigate to={'/login'} state={{ from: location }} replace></Navigate>;
+};
+
+export default PrivateRoute;
---
+++ src/Routers/Route.js
@@ -0,0 +1,42 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import HomePage from "../Pages/Home/HomePage";
+import LoginPage from "../Pages/LoginPage/LoginPage";
+import SignUp from "../Pages/SignUpPage/SignUp";
+import DashboardLayout from "../Layouts/DashboardLayout";
+import AllUsers from "../Pages/AdminPanel/Allusers";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <HomePage></HomePage>
+            },
+            {
+                path: '/logIn',
+                element: <LoginPage></LoginPage>
+            },
+            {
+                path: '/signUp',
+                element: <SignUp></SignUp>
+            }
+        ]
+    },
+    // {
+    //     path: 'dashboard',
+    //     element: <DashboardLayout></DashboardLayout>,
+    //     children: [
+    //         {
+    //             path: '/dashboard/allUsers',
+    //             element: <AllUsers></AllUsers>
+    //         }
+    //     ]
+    // }
+]);
+
+export default router
