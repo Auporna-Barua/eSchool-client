e6bce83e870c37df325ec8ce20611c4b6c0c9cdb
Auporna-Barua <aupornabarua@gmail.com>
2023-06-16 02:04:49 +0600

    class page some bug fix

--- src/components/Classes/Class.jsx
+++ src/components/Classes/Class.jsx
@@ -3,9 +3,13 @@ import { AiOutlineCalendar, AiOutlineHeart } from 'react-icons/ai'
 import { FcManager } from 'react-icons/fc'
 import Swal from 'sweetalert2';
 import { AuthContext } from '../../Provider/AuthProvider';
+import { Link } from 'react-router-dom';
+import useAdmin from '../../hooks/useAdmin';
 
 function Class({ course }) {
     const { user } = useContext(AuthContext);
+    const isAdmin = useAdmin();
+    console.log("isAdmin 23", isAdmin[1]);
     const token = localStorage.getItem('access-token');
     const handleSelect = (course) => {
         fetch(`https://e-school-mu.vercel.app/selectedClass/${user.email}`, {
@@ -29,29 +33,32 @@ function Class({ course }) {
             })
     }
     return (
-        <div className="card card-compact  bg-base-100 shadow-xl">
+        <div className={`card card-compact  bg-base-100  shadow-xl ${course?.seats < 1 && "bg-red-600"}`}>
             <figure><img src={course.photo} alt="Shoes" /></figure>
             <div className="card-body">
-                <h2 className="card-title">{course.name}</h2>
+                <h2 className={`card-title ${course?.seats < 1 && "text-white"}`}>{course.name}</h2>
 
                 <div className="card-actions justify-between">
-                    <div className='flex items-center gap-2'><FcManager size={20} /> <span className='text-md font-bold capitalize text-gray-500'>{course.instructor}</span></div>
-                    <div className='flex items-center gap-2'><AiOutlineHeart size={20} color={"#FF7703"} /> <span className='text-md font-bold capitalize text-gray-500'>{course.enroll ? course.enroll : "0"} Student</span></div>
+                    <div className='flex items-center gap-2'><FcManager size={20} /> <span className={`text-md font-bold capitalize text-gray-500 ${course?.seats < 1 && "text-white"}`}>{course.instructor}</span></div>
+                    <div className={`flex items-center gap-2 ${course?.seats < 1 && "text-white" }`}><AiOutlineHeart size={20} color={"#FF7703"} /> <span className='text-md font-bold capitalize text-gray-500'>{course.enroll ? course.enroll : "0"} Student</span></div>
                 </div>
                 <div className="card-actions">
                     <div className='flex items-center gap-2'>
-                        <p className='text-md font-bold capitalize text-gray-500'>Available seats: {course.seats}</p>
+                        <p className={`text-md font-bold capitalize text-gray-500 ${course?.seats < 1 && "text-white"}`}>Available seats: {course.seats}</p>
                     </div>
 
                 </div>
                 <div className="card-actions">
 
-                    <p className='text-md font-bold capitalize text-gray-500'>Price: {course.price}৳</p>
+                    <p className={`text-md font-bold capitalize text-gray-500 ${course?.seats < 1 && "text-white"}`}>Price: {course.price}৳</p>
 
 
                 </div>
                 <div className="card-actions">
-                    <button className="btn bg-[#FF7703] text-white w-full hover:bg-[#FF7703]" onClick={() => handleSelect(course)}>Select Course</button>
+                    {
+                        user && user.email ? <button className={`btn bg-[#FF7703] text-white w-full hover:bg-[#FF7703] ${(course.seats < 1 || isAdmin[1] == "musician" || isAdmin[1] == "admin") && "cursor-not-allowed pointer-events-none	 opacity-30"}`} onClick={() => handleSelect(course)}>Select Course</button> : <Link to="/logIn" className={`btn bg-[#FF7703] text-white w-full hover:bg-[#FF7703]`} >Login for select course</Link>
+                    }
+
 
                 </div>
             </div>
--- src/pages/EnrolledClasses/EnrolledClass.jsx
+++ src/pages/EnrolledClasses/EnrolledClass.jsx
@@ -1,8 +1,112 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import { AiOutlineDelete } from 'react-icons/ai';
+import { GiPayMoney } from 'react-icons/gi';
+
+import Swal from 'sweetalert2';
+import { AuthContext } from '../../Provider/AuthProvider';
+import Tittle from '../../components/metaTitle/Title';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import { Link } from 'react-router-dom';
 
 function EnrolledClass() {
+  const token = localStorage.getItem('access-token');
+  const { user } = useContext(AuthContext);
+
+  const { data: classes = [], refetch } = useQuery(['selectedClass'], async () => {
+    const res = await fetch(`https://e-school-mu.vercel.app/enrolledClasses/${user?.email}`, {
+      headers: {
+        authorization: `bearer ${token}`,
+        'content-type': 'application/json',
+
+      }
+    })
+    return res.json();
+  })
+  const handleDelete = (id) => {
+    fetch(`https://e-school-mu.vercel.app/selectedClass/${id}`, {
+      method: 'DELETE',
+      headers: {
+        authorization: `bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (data.deletedCount == 1) {
+          refetch();
+          Swal.fire({
+            icon: 'success',
+            text: 'Unselected successful',
+          })
+
+        };
+      })
+  }
+
+
+
   return (
-    <div>EnrolledClass</div>
+    <div>
+      <Tittle heading={"My Enrolled Classes"} />
+      <div className=''>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+
+                <th>Class Name</th>
+                <th>Instructor</th>
+                <th>Price</th>
+                <th>Payment</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {
+                classes.length > 0 ? classes.map(course => <tr key={course._id}>
+                  <td>
+                    <div className="flex items-center space-x-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                          <img src={course?.photo} alt="Avatar Tailwind CSS Component" />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{course.name.length > 25 ? course.name.substring(0, 25) + "..." : course.name}</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    {course.instructor}
+                    <br />
+                    <span className="badge badge-ghost badge-sm">{course.email}</span>
+                  </td>
+
+                  <td className='text-md font-bold'>{course?.price}৳</td>
+
+                  <td>
+                    <Link to={`/dashboard/payment/${course._id}`}>
+                      <button className={`btn btn-sm text-white bg-red-500 rounded-full`} >{course?.paymentStatus}</button>
+                    </Link>
+
+                  </td>
+              
+
+                </tr>)
+                  : <h2 className='text-3xl mt-10 block text-center'>No enrolled Item here</h2>
+              }
+
+            </tbody>
+
+
+          </table>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
